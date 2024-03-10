6ba4ddfc5f6fdb1297cfc33f8c69035c24770da0
wei <wendyhsu09@gmail.com>
2024-03-11 00:32:38 +0800

    修改少寫內容

--- api/add_poster.php
+++ api/add_poster.php
@@ -2,6 +2,7 @@
 <?php
 include_once "db.php";
 if (isset($_FILES['poster']['tmp_name'])) {
+    move_uploaded_file($_FILES['poster']['tmp_name'], "../img/{$_FILES['poster']['name']}");
     $_POST['img'] = $_FILES['poster']['name'];
 }
 $_POST['sh'] = 1;
